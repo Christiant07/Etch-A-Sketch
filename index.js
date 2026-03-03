1989e623565f3d49d7dfc97dc18b496480d198df
christian <christiant_07@berkeley.edu>
2026-03-02 16:19:33 -0800

    added action to the buttons and hardcoded the grid

--- index.js
+++ index.js
@@ -15,18 +15,85 @@ Requiremmets:
 //document get element by id for the column and style it to string styling
 //same for rows
 
-let size = prompt("Enter a Height and Width:");
-console.log(size);
-createGrid(size);
+// let size = prompt("Enter a Height and Width:");
+// console.log(size);
+createGrid(10);
+let px = 500/size;
+
+
+let gridPieces = document.querySelectorAll(".gridpiece");//returns a list of every gridpice in our grid box
+gridPieces.forEach((i) => i.addEventListener("mouseover", chooser)) //applies a listener to every elemnt where if we hover over it we apply some function
+ 
+// function coloring() {
+//     console.log("reached")
+//     this.style.background = "pink";
+//     console.log(this.style.background)
+// }
+
+// function erase() {
+//     this.style.background = "white";
+// }
+
+function reset() {
+    gridPieces.forEach((i) => i.style.background = "white")//for evryr element in our grid box, set eahc grid piece to white
+}
+
+let switcher = null;
+let erasing = document.getElementById("eraser");
+let pen = document.getElementById("pen");
+let reseter = document.getElementById("reset");
+erasing.addEventListener("click", () => {switcher = 0})
+pen.addEventListener("click", () => {switcher = 1})
+reseter.addEventListener("click", reset)
+
+function chooser() {
+    if (switcher == 0) {
+        this.style.background = "white";;
+    } else {
+        this.style.background = "pink";
+    }
+}
 
 function createGrid(num) {
-    string = "repeat(" + num + "," + "px)";
-    document.getElementById("grid").style.gridTemplateColumns = string;
-    document.getElementById("grid").style.gridTemplateRows = string;
+    // string = "repeat(" + num + (", " + 33 + "px)");
+    // document.getElementById("grid").style.gridTemplateColumns = string;
+    // document.getElementById("grid").style.gridTemplateRows = string;
+    let gridBox = document.getElementById("grid")
     for (let i = 0; i < num*num; i++) {
         div = document.createElement("div");
-        div.classList.add("grid-");
+        div.classList.add("gridpiece");
         div.style.borderWidth = "3px";
         div.style.border = "solid black";
+        gridBox.appendChild(div);//appends every div we created into the our grid box or grid from html
     }
 } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 'use client';
+
+// type ButtonProps = {
+//     text: string;
+//     color: string;
+//     fontSize: number;
+//   };
+  
+//   export default function Button({ text, color, fontSize }: ButtonProps) {
+//     const handleClick = () => alert(`Clicked ${text}`);
+//     return (
+//       <button onClick={handleClick} style={{ color, fontSize: `${fontSize}px` }}>
+//         {text}
+//       </button>
+//     );
+//   }
+  
